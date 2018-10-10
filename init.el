e36b9c4a5415fe8717cec82e2e36068ece611dd6
Hugo Blanco Sandoval <white.thief@gmail.com>
2018-10-10 10:21:02 -0700

    A bunch of things that I forgot to commit

--- init.el
+++ init.el
@@ -5,12 +5,23 @@
 ;; Disable welcome screen
 (setq inhibit-startup-screen t)
 
+;; Ask for emacs to confirm exit
+(setq confirm-kill-emacs 'y-or-n-p)
+
+;; disable ctrl-x ctrl-z that send emacs to the background
+(put 'suspend-frame 'disabled t)
+
 ;; enable line number for all!
 (global-linum-mode t)
 
 ;; yes/no prompts to y/n
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Ctrl-h map to delete-backward
+(global-set-key (kbd "C-?") 'help-command)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+                
 ;; backup/auto-save files
 (setq
   backup-by-copying t      ; don't clobber symlinks
@@ -22,17 +33,22 @@
   kept-old-versions 2
   version-control t)       ; use versioned backups
 
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs.d/backups" t)))
+
+;; disable the lockfiles
+(setq create-lockfile nil)
+
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
 
 ;; change tabs to 2 spaces width
+(setq-default indent-tabs-mode nil) ;; use spaces, no tab chars
 (setq tab-width 2)
 (setq default-tab-width 2)
 (setq-default js-indent-level 2)
 (setq-default js2-basic-offset 2)
 
-;; Ctrl-h map to delete-backward
-(keyboard-translate ?\C-h ?\C-?);
 ;; Setting my theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 ;;(load-theme 'weyland-yutani t)
@@ -76,6 +92,8 @@
 (global-company-mode)
 (add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook      #'company-mode)
+;; This enables case sensitive completions => https://goo.gl/MBqNu6
+(setq company-dabbrev-downcase nil)
 
 (require 'clojure-mode)
 (add-hook 'clojure-mode-hook 'cider) ;; enter cider mode when entering clojure-mode
@@ -95,6 +113,12 @@
 (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
 (define-key js-mode-map (kbd "M-.") nil) ;; js-mode binds "M-." which conflicts with xref, so unbinding
 (add-hook 'js2-mode-hook (lambda () (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+ 
+(require 'highlight-symbol)
+(global-set-key [(control f3)] 'highlight-symbol)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -103,7 +127,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (exec-path-from-shell json-mode web-mode coffee-mode feature-mode treemacs jade-mode pug-mode company-shell tide reykjavik-theme rjsx-mode js2-refactor xref-js2 js2-mode emmet-mode flycheck flycheck-inline rainbow-delimiters company clojure-mode))))
+    (highlight-symbol treemacs cider markdown-mode markdown-mode+ markdown-preview-mode exec-path-from-shell json-mode web-mode coffee-mode feature-mode jade-mode pug-mode company-shell tide reykjavik-theme rjsx-mode js2-refactor xref-js2 js2-mode emmet-mode flycheck flycheck-inline rainbow-delimiters company clojure-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
